:bug: resolve LQR control issue. Mainly lies in quaternion conversion from (w,x,y,z) to (x,y,z,w)

quadjax/controllers/lqr.py
@@ -28,13 +28,20 @@ class LQRController:
         u = jnp.array([env_params.m * env_params.g, 0.0, 0.0, 0.0])
         A = self.A_func(self.env.equib, u, env_params, env_params.dt)
         B = self.B_func(self.env.equib, u, env_params, env_params.dt)
+        # verify controllability
+        # print(np.linalg.matrix_rank(control.ctrb(np.array(A), np.array(B))))
+
         A_reduced = self.E_q0.T @ A @ self.E_q0
         B_reduced = self.E_q0.T @ B
+        # save A_reduced and B_reduced to csv
+        np.savetxt("../../results/A.csv", A_reduced, delimiter=",")
+        np.savetxt("../../results/B.csv", B_reduced, delimiter=",")
         # solve discrete time LQR to get K
         dlqr_args = (A_reduced, B_reduced, control_params.Q, control_params.R)
         dlqr_args = [np.asarray(item) for item in dlqr_args]
         # verify controllability
-        print(np.linalg.matrix_rank(control.ctrb(*dlqr_args[:2])))
+        # print(np.linalg.matrix_rank(control.ctrb(*dlxqr_args[:2])))
+        # get control parameters
         K, _, _ = control.dlqr(*dlqr_args)
         # update controller parameters
         control_params = control_params.replace(K=K)
@@ -51,4 +58,10 @@ class LQRController:
         delta_x = jnp.concatenate([delta_pos, delta_phi, delta_v, delta_w])
         u = jnp.array([env_params.m * env_params.g, 0.0, 0.0, 0.0]) - \
             control_params.K @ delta_x
-        return u
+        # normalize u
+        thrust = u[0]
+        torque = u[1:4]
+        thrust_normed = (u[0] / env_params.max_thrust) * 2.0 - 1.0
+        torque_normed = u[1:4] / env_params.max_torque
+        u_normed = jnp.concatenate([jnp.array([thrust_normed]), torque_normed])
+        return u_normed

quadjax/dynamics/free.py
@@ -1,12 +1,14 @@
+import jax
 from jax import numpy as jnp
 
 from quadjax.dynamics.dataclass import EnvParams3D, EnvState3D, Action3D
 from quadjax.dynamics import geom, utils
 
 def get_free_dynamics_3d():
-    H = jnp.vstack((jnp.zeros((1, 3)), jnp.eye(3)))
+    # H = jnp.vstack((jnp.eye(3), jnp.zeros((1, 3))))
 
-    def quad_dynamics(x:jnp.array, u:jnp.array, params: EnvParams3D):
+    @jax.jit
+    def quad_dynamics(x:jnp.ndarray, u:jnp.ndarray, params: EnvParams3D):
         thrust = u[0]
         torque = u[1:4]
 
@@ -18,17 +20,26 @@ def get_free_dynamics_3d():
 
         # dynamics
         r_dot = Q @ v
-        q_dot = 0.5 * geom.L(q) @ H @ omega
-        v_dot = Q.T @ jnp.array([0, 0, -params.g]) + thrust / params.m * jnp.array([0, 0, 1]) - jnp.cross(omega, v)
-        omega_dot = jnp.linalg.inv(params.I) @ (torque - jnp.cross(omega, params.I @ omega))
+        q_dot = 0.5 * geom.L(q) @ geom.H @ omega
+        v_dot = Q.T @ jnp.asarray([0, 0, -params.g]) + thrust / params.m * jnp.asarray([0, 0, 1]) - geom.hat(omega) @ v
+        omega_dot = jnp.linalg.inv(params.I) @ (torque - geom.hat(omega) @ params.I @ omega)
 
         # return
         x_dot = jnp.concatenate([r_dot, q_dot, v_dot, omega_dot])
         return x_dot
 
-    quad_dynamics_rk4 = lambda x, u, params, dt: utils.rk4(quad_dynamics, x, u, params, dt)
+    @jax.jit
+    def quad_dynamics_rk4(x: jnp.ndarray, u: jnp.ndarray, params: EnvParams3D, dt: float) -> jnp.ndarray:    
+        f = quad_dynamics
+        k1 = f(x, u, params)
+        k2 = f(x + k1 * dt / 2, u, params)
+        k3 = f(x + k2 * dt / 2, u, params)
+        k4 = f(x + k3 * dt, u, params)
+        # x_new = x + (k1 + 2 * k2 + 2 * k3 + k4) / 6 * dt
+        x_new = x + k1 * dt
+        return x_new.at[3:7].set(x_new[3:7] / jnp.linalg.norm(x_new[3:7]))
 
-    # dynamics (params, states) -> states_dot
+    @jax.jit
     def free_dynamics_3d(env_params: EnvParams3D, env_state: EnvState3D, env_action: Action3D):
         # dynamics
         u = jnp.concatenate([jnp.array([env_action.thrust]), env_action.torque])

quadjax/dynamics/geom.py
@@ -32,45 +32,52 @@ def rotate_with_quat(v: jnp.ndarray, quat: jnp.ndarray) -> jnp.ndarray:
     return v_rot[:3]
 
 # Quaternion functions
+@jax.jit
 def hat(v: jnp.ndarray) -> jnp.ndarray:
     return jnp.array([[0, -v[2], v[1]],
                      [v[2], 0, -v[0]],
                      [-v[1], v[0], 0]])
 
+@jax.jit
 def L(q: jnp.ndarray) -> jnp.ndarray:
     '''
-    L(q) = [s, -v; v^T, sI + hat(v)]
+    L(q) = [sI + hat(v), v; -v^T, s]
     left multiplication matrix of a quaternion
     '''
     s = q[3]
     v = q[:3]
-    upper = jnp.hstack((s, -v))
-    lower_left = v.reshape(-1, 1)
-    lower_right = s * jnp.eye(3) + hat(v)
-    lower = jnp.hstack((lower_left, lower_right))
-    return jnp.vstack((upper, lower))
+    right = jnp.hstack((v, s)).reshape(-1, 1)
+    left_up = s * jnp.eye(3) + hat(v)
+    left_down = -v
+    left = jnp.vstack((left_up, left_down))
+    return jnp.hstack((left, right))
 
-def E(q):
+H = jnp.vstack((jnp.eye(3), jnp.zeros((1, 3))))
+
+@jax.jit
+def E(q: jnp.ndarray)->jnp.ndarray:
     '''
     reduced matrix for quadrotor state
     '''
     I3 = jnp.eye(3)
     I6 = jnp.eye(6)
-    H = jnp.vstack((jnp.zeros((1, 3)), jnp.eye(3)))
     G = L(q) @ H
     return jax.scipy.linalg.block_diag(I3, G, I6)
 
+@jax.jit
 def qtoQ(q: jnp.ndarray) -> jnp.ndarray:
     '''
     covert a quaternion to a 3x3 rotation matrix
     '''
     T = jnp.diag(jnp.array([-1, -1, -1, 1]))
-    H = jnp.vstack((jnp.zeros((1, 3)), jnp.eye(3))) # used to convert a 3d vector to 4d vector
+    # H = jnp.vstack((jnp.eye(3), jnp.zeros((1, 3)))) # used to convert a 3d vector to 4d vector
     Lq = L(q)
     return H.T @ T @ Lq @ T @ Lq @ H
 
-def rptoq(phi):
+@jax.jit
+def rptoq(phi: jnp.ndarray) -> jnp.ndarray:
     return 1/jnp.sqrt(1+jnp.dot(phi, phi))*jnp.concatenate((phi, jnp.array([1])))
 
-def qtorp(q):
+@jax.jit
+def qtorp(q: jnp.ndarray) -> jnp.ndarray:
     return q[:3]/q[3]

quadjax/envs/quad3d_free.py
@@ -26,7 +26,7 @@ class Quad3D(environment.Environment):
     github.com/openai/gym/blob/master/gym/envs/classic_control/Quad3D.py
     """
 
-    def __init__(self, task: str = "tracking", dynamics: str = 'free'):
+    def __init__(self, task: str = "hovering", dynamics: str = 'free'):
         super().__init__()
         self.task = task
         # reference trajectory function
@@ -127,7 +127,7 @@ class Quad3D(environment.Environment):
         zeros3 = jnp.zeros(3)
         state = EnvState3D(
             # drone
-            pos=pos_traj[0],
+            pos=zeros3,
             vel=zeros3,
             omega=zeros3,
             quat=jnp.concatenate([zeros3, jnp.array([1.0])]),