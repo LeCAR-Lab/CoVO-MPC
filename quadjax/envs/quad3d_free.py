:bug: resolve LQR control issue. Mainly lies in quaternion conversion from (w,x,y,z) to (x,y,z,w)

quadjax/envs/quad3d_free.py
@@ -262,7 +262,7 @@ def main(args: Args):
     # with jax.disable_jit():
     control_params = controllers.LQRParams(
         Q = jnp.diag(jnp.ones(12)),
-        R = jnp.diag(jnp.array([0.8, 9e-3, 9e-3, 2e-3])),
+        R = 0.01 * jnp.diag(jnp.ones(4)),
         K = jnp.zeros((4, 12)),
     )
     controller = controllers.LQRController(env)