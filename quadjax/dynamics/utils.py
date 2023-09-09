07b56ad1396d34b64c388f34bfb82c9a9dda27c3
jc-bao <jc-bao@outlook.com>
2023-09-09 16:29:37 -0400

    :zap: update reward function

--- quadjax/dynamics/utils.py
+++ quadjax/dynamics/utils.py
@@ -168,12 +168,13 @@ def hovering_reward_fn(state: EnvState3D):
 def tracking_reward_fn(state: EnvState3D):
     err_pos = jnp.linalg.norm(state.pos_tar - state.pos)
     err_vel = jnp.linalg.norm(state.vel_tar - state.vel)
-    reward = 0.8 - \
+    reward = 0.9 - \
         0.05 * err_vel - \
         err_pos * 0.4 - \
         jnp.clip(jnp.log(err_pos + 1) * 4, 0, 1) * 0.4 - \
         jnp.clip(jnp.log(err_pos + 1) * 8, 0, 1) * 0.2 - \
-        jnp.clip(jnp.log(err_pos + 1) * 16, 0, 1) * 0.1
+        jnp.clip(jnp.log(err_pos + 1) * 16, 0, 1) * 0.1 - \
+        jnp.clip(jnp.log(err_pos + 1) * 32, 0, 1) * 0.1
 
     return reward
 
