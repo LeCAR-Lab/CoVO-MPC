4cb8827e7b1f72f283ae02c80c48adbe8b02683d
jc-bao <jc-bao@outlook.com>
2023-10-04 20:23:42 -0400

    :zap: add lower level PI controller

--- quadjax/dynamics/dataclass.py
+++ quadjax/dynamics/dataclass.py
@@ -167,7 +167,7 @@ class EnvParams3D:
     max_speed: float = 8.0
     max_torque: jnp.ndarray = default_array([9e-3, 9e-3, 2e-3])
     # max_torque: jnp.ndarray = default_array([2e-3, 2e-3, 0.5e-3])
-    max_omega: jnp.ndarray = default_array([60.0, 60.0, 10.0])
+    max_omega: jnp.ndarray = default_array([20.0, 20.0, 3.0])
     max_thrust: float = 0.8
     dt: float = 0.02
     g: float = 9.81  # gravity
