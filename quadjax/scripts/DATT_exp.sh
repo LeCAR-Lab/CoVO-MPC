3814e5b44b61014f56175622ef51b08d2e6ddd72
jc-bao <jc-bao@outlook.com>
2023-10-14 23:34:05 -0400

    :zap: init DATT

--- quadjax/scripts/DATT_exp.sh
+++ quadjax/scripts/DATT_exp.sh
@@ -1,2 +1,2 @@
 # DATT
-python ../train.py --env quad3d_free --dynamics bodyrate --noDR --name DATT --lower_controller l1 --debug # --obs_type quad_l1  --debug
+python ../train.py --env quad3d_free --dynamics bodyrate --noDR --name DATT --lower_controller l1 # --obs_type quad_l1  --debug
