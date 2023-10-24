31b27d7581d07e76bb6655790988fdf93de454d9
WENLIXIAO-CS <randyxiao64@gmail.com>
2023-10-23 21:41:58 -0400

    can assign filename now

--- quadjax/test/vis.py
+++ quadjax/test/vis.py
@@ -5,6 +5,13 @@ from meshcat.animation import Animation, convert_frames_to_video
 import numpy as np
 import time
 import pickle
+from absl import app
+from absl import flags
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string('name', '', 'Task name for the visualization.')
+
+
 
 # Create a Meshcat visualizer
 vis = meshcat.Visualizer()
@@ -49,42 +56,46 @@ def set_frame(i, name, transform):
     with anim.at_frame(vis, i) as frame:
         frame[name].set_transform(transform)
 
-# Add a box to the scene
-box = g.Box([1, 1, 1])
-vis["drone"].set_object(g.StlMeshGeometry.from_file('../assets/crazyflie2.stl'))
-vis["drone_frame"].set_object(g.StlMeshGeometry.from_file('../assets/axes.stl'))
-vis["obj"].set_object(g.Sphere(0.02), material=g.MeshLambertMaterial(color=0x0000ff))
-vis["obj_tar"].set_object(g.Sphere(0.02), material=g.MeshLambertMaterial(color=0xff0000))
-vis["disturb"].set_object(g.StlMeshGeometry.from_file('../assets/arrow.stl'))
+def main(argv):
+    # Add a box to the scene
+    box = g.Box([1, 1, 1])
+    vis["drone"].set_object(g.StlMeshGeometry.from_file('../assets/crazyflie2.stl'))
+    vis["drone_frame"].set_object(g.StlMeshGeometry.from_file('../assets/axes.stl'))
+    vis["obj"].set_object(g.Sphere(0.02), material=g.MeshLambertMaterial(color=0x0000ff))
+    vis["obj_tar"].set_object(g.Sphere(0.02), material=g.MeshLambertMaterial(color=0xff0000))
+    vis["disturb"].set_object(g.StlMeshGeometry.from_file('../assets/arrow.stl'))
 
-# create a circle with small capacity
-vis["circle"].set_object(g.Cylinder(0.02, 0.11), material=g.MeshLambertMaterial(color=0x0000ff))
-vis["circle"].set_transform(tf.rotation_matrix(np.pi/2, [0, 0, 1]))
+    # create a circle with small capacity
+    vis["circle"].set_object(g.Cylinder(0.02, 0.11), material=g.MeshLambertMaterial(color=0x0000ff))
+    vis["circle"].set_transform(tf.rotation_matrix(np.pi/2, [0, 0, 1]))
 
-for i in range(0, 300, 2):
-    vis[f"traj{i}"].set_object(g.Sphere(0.01), material=g.MeshLambertMaterial(color=0x00ff00))
+    for i in range(0, 300, 2):
+        vis[f"traj{i}"].set_object(g.Sphere(0.01), material=g.MeshLambertMaterial(color=0x00ff00))
 
-# load state sequence from pickle and check if load is successful
-file_path = "../../results/state_seq_.pkl"
-# file_path = "../../results/state_seq_quad3d_free_jumping_l1_bodyrate.pkl"
-with open(file_path, "rb") as f:
-    state_seq = pickle.load(f)
+    # load state sequence from pickle and check if load is successful
+    file_path = "../../results/state_seq_.pkl"
+    # file_path = "../../results/state_seq_quad3d_free_jumping_l1_bodyrate.pkl"
+    with open(file_path, "rb") as f:
+        state_seq = pickle.load(f)
 
-# visualize the trajectory
-# with anim.at_frame(vis, 0) as frame:
-#     vis_traj(state_seq[0].pos_traj,state_seq[0].vel_traj)
+    # visualize the trajectory
+    # with anim.at_frame(vis, 0) as frame:
+    #     vis_traj(state_seq[0].pos_traj,state_seq[0].vel_traj)
 
-# Apply the transformations according to the time sequence
-for i, state in enumerate(state_seq):
-    if i % 20 == 0:
-        # plot the trajectory by connecting the points in traj_x with lines
-        for j in range(0, 300, 2):
-            set_frame(i, f'traj{j}', pos_quat_to_transform(state_seq[i]['pos_traj'][j], np.array([0,0,0,1])))
-    set_frame(i, 'drone', pos_quat_to_transform(state['pos'], state['quat']))
-    set_frame(i, 'drone_frame', pos_quat_to_transform(state['pos'], state['quat']))
-    set_frame(i, 'obj', pos_quat_to_transform(state['pos_obj'], np.array([0,0,0,1])))
-    set_frame(i, 'obj_tar', pos_quat_to_transform(state['pos_tar'], np.array([0,0,0,1])))
-    set_frame(i, 'disturb', origin_vec_to_transform(state['pos'], state['f_disturb'], 2.0))
-    
-vis.set_animation(anim)
-time.sleep(5)
+    # Apply the transformations according to the time sequence
+    for i, state in enumerate(state_seq):
+        if i % 20 == 0:
+            # plot the trajectory by connecting the points in traj_x with lines
+            for j in range(0, 300, 2):
+                set_frame(i, f'traj{j}', pos_quat_to_transform(state_seq[i]['pos_traj'][j], np.array([0,0,0,1])))
+        set_frame(i, 'drone', pos_quat_to_transform(state['pos'], state['quat']))
+        set_frame(i, 'drone_frame', pos_quat_to_transform(state['pos'], state['quat']))
+        set_frame(i, 'obj', pos_quat_to_transform(state['pos_obj'], np.array([0,0,0,1])))
+        set_frame(i, 'obj_tar', pos_quat_to_transform(state['pos_tar'], np.array([0,0,0,1])))
+        set_frame(i, 'disturb', origin_vec_to_transform(state['pos'], state['f_disturb'], 2.0))
+        
+    vis.set_animation(anim)
+    time.sleep(5)
+
+if __name__ == '__main__':
+  app.run(main)
