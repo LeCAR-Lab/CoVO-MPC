81ec50fffbffe183d20ec301c2e7e239fa1e305d
jc-bao <jc-bao@outlook.com>
2023-11-01 11:52:01 -0400

    :zap: match the sampling volume of different methods

--- quadjax/scripts/quad2d_free_mppi_sensitivity.sh
+++ quadjax/scripts/quad2d_free_mppi_sensitivity.sh
@@ -1,15 +1,15 @@
 task=tracking_zigzag
 
-# for controller in mppi
-# do
-#     for N in 2 4 8 16 32 64 128 256 512 1024
-#     do
-#         echo "Running with N = $N"
-#         python ../envs/quad2d_free.py --task ${task} --dynamics bodyrate --controller ${controller} --mode eval --controller_params "N${N}_H32_lam0.01"
-#     done
-# done
+for controller in mppi
+do
+    for N in 2 4 8 16 32 64 128 256 512 1024
+    do
+        echo "Running with N = $N"
+        python ../envs/quad2d_free.py --task ${task} --dynamics bodyrate --controller ${controller} --mode eval --controller_params "N${N}_H32_lam0.01"
+    done
+done
 
-N=10
+N=1024
 for controller in mppi mppi_zeji_mean
 do
     for H in 2 4 8 16 32 64 128
